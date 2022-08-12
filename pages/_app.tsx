7e9f7cb4edc34a433af618a27ca072e88fc89195
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-12 17:55:57 +0200

    Add theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../src/styles/globals.css";
 import type { AppProps } from "next/app";
+import { theme } from "@damianveltkamp/dvds";
+import { ThemeProvider } from "@emotion/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  console.log(theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
