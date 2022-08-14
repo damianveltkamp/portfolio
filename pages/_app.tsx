4af228c9ab28e3f0376f2e51678c0e447c1fcea7
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-14 18:33:55 +0200

    Added header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,12 @@
-import styled from "@emotion/styled";
 import type { AppProps } from "next/app";
-import { Footer, MainContainer, theme } from "@damianveltkamp/dvds";
+import { Footer, Header, MainContainer, theme } from "@damianveltkamp/dvds";
 import { ThemeProvider } from "@emotion/react";
-import DamianLogo from "../src/icons/logo.svg";
-import GithubLogo from "../src/icons/github-brands.svg";
-import { Icon } from "../src/components/Icon/Icon";
 import "../src/styles/globals.css";
-
-const HeaderContainer = styled("header")`
-  display: flex;
-  padding: 20px;
-  align-items: center;
-  justify-content: space-between;
-`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <HeaderContainer>
-        <a
-          aria-label="Damian Veltkamp logo"
-          href="https://www.damianveltkamp.tech/"
-        >
-          <Icon width="45px" icon={DamianLogo} />
-        </a>
-        <a
-          aria-label="Github logo"
-          href="https://github.com/damianveltkamp"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Icon icon={GithubLogo} />
-        </a>
-      </HeaderContainer>
+      <Header />
       <MainContainer>
         <Component {...pageProps} />
       </MainContainer>
