602a168891345f3ea1a59780b591f5022fab1dc2
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-14 20:25:17 +0200

    Added global styles

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import styled from "@emotion/styled";
+import { Global } from "@emotion/react";
+import { globalStyles } from "@damianveltkamp/dvds";
 
 const HtmlContainer = styled(Html)`
   background-color: #1f2937;
@@ -8,68 +10,71 @@ const HtmlContainer = styled(Html)`
 
 export default function Document() {
   return (
-    <HtmlContainer>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
+    <>
+      <Global styles={globalStyles} />
+      <HtmlContainer>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
 
-        <link
-          rel="preload"
-          href="/fonts/roboto/Roboto-Thin.ttf"
-          as="font"
-          crossOrigin=""
-        />
+          <link
+            rel="preload"
+            href="/fonts/roboto/Roboto-Thin.ttf"
+            as="font"
+            crossOrigin=""
+          />
 
-        <link
-          rel="preload"
-          href="/fonts/roboto/Roboto-Regular.ttf"
-          as="font"
-          crossOrigin=""
-        />
+          <link
+            rel="preload"
+            href="/fonts/roboto/Roboto-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
 
-        <link
-          rel="preload"
-          href="/fonts/roboto/Roboto-Medium.ttf"
-          as="font"
-          crossOrigin=""
-        />
+          <link
+            rel="preload"
+            href="/fonts/roboto/Roboto-Medium.ttf"
+            as="font"
+            crossOrigin=""
+          />
 
-        <link
-          rel="preload"
-          href="/fonts/roboto/Roboto-Bold.ttf"
-          as="font"
-          crossOrigin=""
-        />
+          <link
+            rel="preload"
+            href="/fonts/roboto/Roboto-Bold.ttf"
+            as="font"
+            crossOrigin=""
+          />
 
-        <link rel="stylesheet" type="text/css" href="/fonts/fonts.css" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#1f2937" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </HtmlContainer>
+          <link rel="stylesheet" type="text/css" href="/fonts/fonts.css" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#1f2937" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </HtmlContainer>
+    </>
   );
 }
