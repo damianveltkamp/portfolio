0f87ed3f1f9139229ec1570a017b76824645f020
Damian Veltkamp <damian.veltkamp@deptagency.com>
2022-08-18 16:05:55 +0200

    Updated dvds and added skip to content link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../src/styles/fonts.css";
 import type { AppProps } from "next/app";
-import { Footer, Header, MainContainer, theme } from "@damianveltkamp/dvds";
+import {
+  Footer,
+  Header,
+  MainContainer,
+  SkipToContent,
+  theme,
+} from "@damianveltkamp/dvds";
 import { ThemeProvider } from "@emotion/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <SkipToContent text="Skip to main content" skipId="#main" />
       <Header />
       <MainContainer>
         <Component {...pageProps} />
